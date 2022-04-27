db382f8272f523d586f4fe7134096eef5ba169ba
acpaulsen <apaulsen43@gmail.com>
2022-04-26 22:27:17 -0400

    with databse file from lecture

---
+++ database.js
@@ -0,0 +1,19 @@
+const dbs = require('better-sqlite3')
+const db = new dbs('log.db')
+const sql = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name= 'accesslog'`);
+
+let row = sql.get();
+if (row == undefined) {
+    console.log('Log database appears to be empty. Creating log database...')
+    const sqlInit = `
+    CREATE TABLE accesslog ( id INTERGER PRIMARY KEY, remoteaddr VARCHAR, remoteuser VARCHAR, time VARCHAR, method VARCHAR, 
+        url VARCHAR, protocol VARCHAR, httpversion NUMERIC, status INTEGER, referer VARCHAR, useragent VARCHAR);
+    `
+
+    db.exec(sqlInit)
+}
+else {
+    console.log('Log database exists.')
+}
+
+module.exports = db
